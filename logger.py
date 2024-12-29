30fcc91ead3b51dd2eef8a67fa937c89b2a207d5
Filip Linsbauer <linsbauer.fi@gmail.com>
2024-12-29 22:28:32 +0100

    FirstServerAttempt

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
 
-def log_message():
+def log_message(message="LOG SUCCESSFULLY ADDED"):
     with open("log.txt", "a") as f:
-        f.write(datetime.today().strftime('%Y-%m-%d_%H-%M-%S') + " LOG SUCEFULLY ADDED\n")
+        f.write(datetime.today().strftime('%Y-%m-%d_%H-%M-%S') + " " + message + "\n")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from datetime import datetime
 from logger import log_message
+from bluetooth_server import start_bluetooth_server
 
 log_message()
+start_bluetooth_server()
