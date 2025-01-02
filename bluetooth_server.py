e30997583df5c4e3839f76ab1112196b08d168d4
Filip Linsbauer <linsbauer.fi@gmail.com>
2025-01-02 15:43:12 +0100

    ex

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -77,43 +77,47 @@ def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs)
 
 
 async def run(loop):
-    trigger.clear()
-    # Instantiate the server
-    my_service_name = "Test Service"
-    server = BlessServer(name=my_service_name, loop=loop)
-    server.read_request_func = read_request
-    server.write_request_func = write_request
+    try:    
+        trigger.clear()
+        # Instantiate the server
+        my_service_name = "Test Service"
+        server = BlessServer(name=my_service_name, loop=loop)
+        server.read_request_func = read_request
+        server.write_request_func = write_request
 
-    # Add Service
-    my_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
-    await server.add_new_service(my_service_uuid)
+     # Add Service
+        my_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
+        await server.add_new_service(my_service_uuid)
 
-    # Add a Characteristic to the service
-    my_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    char_flags = (
-        GATTCharacteristicProperties.read
-        | GATTCharacteristicProperties.write
-        | GATTCharacteristicProperties.indicate
-    )
-    permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
-    await server.add_new_characteristic(
-        my_service_uuid, my_char_uuid, char_flags, None, permissions
-    )
+        # Add a Characteristic to the service
+        my_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+        char_flags = (
+            GATTCharacteristicProperties.read
+            | GATTCharacteristicProperties.write
+            | GATTCharacteristicProperties.indicate
+        )
+        permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
+        await server.add_new_characteristic(
+            my_service_uuid, my_char_uuid, char_flags, None, permissions
+        )
 
-    log_event(server.get_characteristic(my_char_uuid))
-    await server.start()
-    log_event("Advertising")
-    if trigger.__module__ == "threading":
-        trigger.wait()
-    else:
-        await trigger.wait()
+        log_event(server.get_characteristic(my_char_uuid))
+        await server.start()
+        log_event("Advertising")
+        if trigger.__module__ == "threading":
+            trigger.wait()
+        else:
+            await trigger.wait()
 
-    await asyncio.sleep(2)
-    log_event("Updating")
-    server.get_characteristic(my_char_uuid)
-    server.update_value(my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
-    await asyncio.sleep(5)
-    await server.stop()
+        await asyncio.sleep(2)
+        log_event("Updating")
+        server.get_characteristic(my_char_uuid)
+        server.update_value(my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
+        await asyncio.sleep(5)
+        await server.stop()
+    except(Exception) as e:
+        log_event(f"Error: {e}")
+        print(f"Error: {e}")
 
 def start():
     loop = asyncio.get_event_loop()
