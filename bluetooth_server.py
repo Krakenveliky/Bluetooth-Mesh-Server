9bf73871baf5674b25d6c8c81f2dc048d16e39fd
Filip Linsbauer <linsbauer.fi@gmail.com>
2025-01-02 19:47:11 +0100

    Update bluetooth_server.py

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -90,7 +90,7 @@ async def run(loop):
     try:    
         trigger.clear()
         # Instantiate the server
-        my_service_name = "Test Service"
+        my_service_name = "Filipovo RPI"
         server = BlessServer(name=my_service_name, loop=loop)
         server.read_request_func = read_request
         server.write_request_func = write_request
