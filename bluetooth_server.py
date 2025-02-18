6583630af1bbee63b6ec2bf59b74db395b3ab543
Filip Linsbauer <linsbauer.fi@gmail.com>
2025-02-18 19:03:11 +0100

    kk

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -1,12 +1,19 @@
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
 from datetime import datetime
 import os
 
 async def main():
     devices = await BleakScanner.discover()
     for d in devices:
+        
         log_event(d)
+
+    message = "xd xd"
+    await connect_and_send_message(HM10_MAC_ADDRESS, message)
+    
+  
+    
 
 def start():
     asyncio.run(main())
@@ -30,3 +37,23 @@ def log_event(event):
         with open(LOG_FILE, "a") as f:
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             f.write(f"{timestamp} - {event}\n")
+
+HM10_MAC_ADDRESS = "2C:CF:67:72:24:54"  
+
+
+CHARACTERISTIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+
+async def connect_and_send_message(mac_address, message):
+
+    async with BleakClient(mac_address) as client:
+        print(f"Connected: {client.is_connected}")
+
+        if client.is_connected:
+           
+            await client.write_gatt_char(CHARACTERISTIC_UUID, message.encode())
+            print(f"Message sent: {message}")
+
+       
+        print("Disconnecting...")
+
+        
